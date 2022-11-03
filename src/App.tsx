22a7a5d397c313d0d7da0826451926be7e7b73a4
Florian TELLIER <florian.tellier02@gmail.com>
2022-11-03 12:04:18 +0100

    first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.scss";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import logo from "./logo.svg";
+import HomePage from "./Pages/home-page";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <Link to="/" className="logo">
+            Muscu App
+          </Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
