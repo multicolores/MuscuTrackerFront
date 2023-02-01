51d4c85e558f8d40270b4b8c55f54160c0b5ce6c
Florian TELLIER <florian.tellier02@gmail.com>
2023-02-01 15:56:50 +0100

    style - create-workout-page: update styles

--- src/Pages/create-workouts.tsx
+++ src/Pages/create-workouts.tsx
@@ -7,140 +7,164 @@ import Notification from "../components/materialUI/Notification";
 import { createWorkout } from "../servicesFunctions/createWorkout";
 import { useCookies } from "react-cookie";
 import { Link } from "react-router-dom";
+import create_workout_image from "../images/create_workout_image.png";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 
 function CreateWorkout() {
-  const [exerciseNumber, setexerciseNumber] = useState(1);
-  const location = useLocation();
-  const [cookies, setCookie] = useCookies(["user"]);
-  const [notify, setNotify] = useState({
-    isOpen: false,
-    message: "",
-    type: "",
-  });
+    const [exerciseNumber, setexerciseNumber] = useState(1);
+    const location = useLocation();
+    const [cookies, setCookie] = useCookies(["user"]);
+    const [notify, setNotify] = useState({
+        isOpen: false,
+        message: "",
+        type: "",
+    });
 
-  console.log(location.state);
-  const user = location.state["user"];
-  //info : There's no difference, location.state["user"] just doesn't get type checked. So, it's more of a workaround  (location.state.user doesn't work)
-  console.log(user);
+    console.log(location.state);
+    const user = location.state["user"];
+    //info : There's no difference, location.state["user"] just doesn't get type checked. So, it's more of a workaround  (location.state.user doesn't work)
+    console.log(user);
 
-  async function CreateTheWorkout(e: any) {
-    // dans le future : go importer cette fonction des services ( pitet plustot la fonction qui fait appelle a l'api avec les bon truc en props dedans)
-    console.log(e);
-    let formArray = [];
-    let exercisesArrayToSend = [];
-    // for (let i = 0; i < e.target.length - 1; i++) {
-    //   formArray.push(e.target[i].value);
-    //   console.log(e.target[i].name);
-    // }
-    for (let i = 1; i < e.target.length - 2; i++) {
-      formArray.push(e.target[i].value);
-      // console.log(e.target[i].name);
+    async function CreateTheWorkout(e: any) {
+        // dans le future : go importer cette fonction des services ( pitet plustot la fonction qui fait appelle a l'api avec les bon truc en props dedans)
+        console.log(e);
+        let formArray = [];
+        let exercisesArrayToSend = [];
+        // for (let i = 0; i < e.target.length - 1; i++) {
+        //   formArray.push(e.target[i].value);
+        //   console.log(e.target[i].name);
+        // }
+        for (let i = 1; i < e.target.length - 2; i++) {
+            formArray.push(e.target[i].value);
+            // console.log(e.target[i].name);
+        }
+        console.log(formArray);
+        // console.log(formArray.length / 4);
+        let separation = 3;
+        for (let i = 0; i < formArray.length / 3; i++) {
+            let objectArray = formArray
+                .slice()
+                .splice(separation - 3, separation);
+            console.log(objectArray);
+            let exerciseObject = {
+                name: objectArray[0],
+                repetition: "",
+                recuperation: objectArray[1],
+                weight: objectArray[2],
+            };
+            exercisesArrayToSend.push(exerciseObject);
+            separation = separation + 3;
+        }
+        console.log(exercisesArrayToSend);
+
+        let workoutObject = {
+            name: e.target[0].value,
+            exercise: exercisesArrayToSend,
+            description: "Description de la scéance",
+        };
+        console.log(workoutObject);
+
+        if (createWorkout(user, workoutObject, cookies.user)) {
+            setNotify({
+                isOpen: true,
+                message: "Workout created !",
+                type: "success",
+            });
+            setexerciseNumber(0);
+        } else {
+            setNotify({
+                isOpen: true,
+                message: "Erreur",
+                type: "error",
+            });
+        }
+        e.preventDefault();
     }
-    console.log(formArray);
-    // console.log(formArray.length / 4);
-    let separation = 3;
-    for (let i = 0; i < formArray.length / 3; i++) {
-      let objectArray = formArray.slice().splice(separation - 3, separation);
-      console.log(objectArray);
-      let exerciseObject = {
-        name: objectArray[0],
-        repetition: "",
-        recuperation: objectArray[1],
-        weight: objectArray[2],
-      };
-      exercisesArrayToSend.push(exerciseObject);
-      separation = separation + 3;
-    }
-    console.log(exercisesArrayToSend);
 
-    let workoutObject = {
-      name: e.target[0].value,
-      exercise: exercisesArrayToSend,
-      description: "Description de la scéance",
-    };
-    console.log(workoutObject);
-
-    if (createWorkout(user, workoutObject, cookies.user)) {
-      setNotify({
-        isOpen: true,
-        message: "Workout created !",
-        type: "success",
-      });
-      setexerciseNumber(0);
-    } else {
-      setNotify({
-        isOpen: true,
-        message: "Erreur",
-        type: "error",
-      });
-    }
-    e.preventDefault();
-  }
-
-  function exercisesInputs() {
-    let inputsDom = [];
-    for (let i = 0; i < exerciseNumber; i++) {
-      inputsDom.push(
-        <div className="exercisesInputs">
-          <label htmlFor={"exoname" + i}>Exercise's Name :</label>
-          <input type="text" name={"exoname" + i} />
-          {/* <label htmlFor={"exoNbSet" + i}>Set number:</label>
+    function exercisesInputs() {
+        let inputsDom = [];
+        for (let i = 0; i < exerciseNumber; i++) {
+            inputsDom.push(
+                <div className="exercisesInputs">
+                    <label htmlFor={"exoname" + i}>Exercise's Name :</label>
+                    <input type="text" name={"exoname" + i} />
+                    {/* <label htmlFor={"exoNbSet" + i}>Set number:</label>
           <input type="text" name={"exoNbSet" + i} /> */}
-          <label htmlFor={"recuperation" + i}>Recuperation :</label>
-          <input type="text" name={"recuperation" + i} />
-          <label htmlFor={"weight" + i}>Weight :</label>
-          <input type="text" name={"weight" + i} />
-        </div>
-      );
+                    <label htmlFor={"recuperation" + i}>Recuperation :</label>
+                    <input type="text" name={"recuperation" + i} />
+                    <label htmlFor={"weight" + i}>Weight :</label>
+                    <input type="text" name={"weight" + i} />
+                </div>
+            );
+        }
+
+        return inputsDom;
     }
+    return (
+        <div className="creatWorkout_container">
+            <Link to="/main" className="btGoBackMainPage">
+                <Button variant="contained" className="multiRoundedButton_white">
+                    <ArrowBackIosNewIcon className="icon-in-button" />
+                </Button>
+            </Link>
+            <div className="form_container">
+                <h1>Create a workout</h1>
+                <form onSubmit={CreateTheWorkout}>
+                    <div>
+                        <label htmlFor="workoutname">Workout's Name :</label>
+                        <input type="text" name="workoutname" />
+                    </div>
 
-    return inputsDom;
-  }
-  return (
-    <div className="creatWorkout_container">
-      <div className="headerContainer">
-        <h1>Create a workout</h1>
-        <Link to="/main" className="btGoBackMainPage">
-          <Button variant="contained" className="gradientButton">
-            Go Back to main page
-          </Button>
-        </Link>
-      </div>
+                    {exercisesInputs()}
+                    <Button variant="contained" className="multiRoundedButton_white submitButton">
+                    <FitnessCenterIcon className="icon-in-button" />
+                        <input
+                            type="submit"
+                            value="Create workout"
+                            className="submitInput"
+                        />
+                    </Button>
+                </form>
 
-      <form onSubmit={CreateTheWorkout}>
-        <div>
-          <label htmlFor="workoutname">Workout's Name :</label>
-          <input type="text" name="workoutname" />
+                <div className="btAddContainer">
+                    <div className="fixed_container">
+                        <Button
+                            variant="contained"
+                            className="multiRoundedButton_white"
+                            onClick={() => {
+                                setexerciseNumber(exerciseNumber + 1);
+                            }}
+                        >
+                            <AddIcon className="icon-in-button" />
+                            Add exercise
+                        </Button>
+                        <Button
+                            variant="contained"
+                            className="multiRoundedButton_white btAddExo"
+                            onClick={() => {
+                                setexerciseNumber(exerciseNumber - 1);
+                            }}
+                        >
+                            <RemoveIcon className="icon-in-button" />
+                        </Button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="right_side_container">
+                <img
+                    src={create_workout_image}
+                    alt="create_workout logo musculation"
+                    className="image_container"
+                />
+            </div>
+
+            <Notification notify={notify} setNotify={setNotify} />
         </div>
-
-        {exercisesInputs()}
-        <Button variant="contained" className="borderButton">
-          <input type="submit" value="Create workout" className="submitInput" />
-        </Button>
-      </form>
-      <div className="btAddContainer">
-        <Button
-          variant="contained"
-          className="btAddExo"
-          onClick={() => {
-            setexerciseNumber(exerciseNumber + 1);
-          }}
-        >
-          Add exercise
-        </Button>
-        <Button
-          variant="contained"
-          className="btAddExo"
-          onClick={() => {
-            setexerciseNumber(exerciseNumber - 1);
-          }}
-        >
-          -
-        </Button>
-      </div>
-      <Notification notify={notify} setNotify={setNotify} />
-    </div>
-  );
+    );
 }
 
 export default CreateWorkout;
--- src/Pages/main-page.tsx
+++ src/Pages/main-page.tsx
@@ -10,101 +10,110 @@ import Workout from "../components/Workout";
 import axios from "axios";
 import { useCookies } from "react-cookie";
 import { Link, useNavigate } from "react-router-dom";
+import LockIcon from "@mui/icons-material/Lock";
+import "../styles/main-page.scss";
 
 function MainPage() {
-  const [data, setData] = useState<any>(null);
-  const [userinfo, setUserinfo] = useState(null);
-  const [workout, setWorkout] = useState<any>(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+    const [data, setData] = useState<any>(null);
+    const [userinfo, setUserinfo] = useState(null);
+    const [workout, setWorkout] = useState<any>(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
-  const [notify, setNotify] = useState({
-    isOpen: false,
-    message: "",
-    type: "",
-  });
+    const [cookies, setCookie, removeCookie] = useCookies(["user"]);
+    const [notify, setNotify] = useState({
+        isOpen: false,
+        message: "",
+        type: "",
+    });
 
-  useEffect(() => {
-    // User data fetching here
-    axios
-      .get(process.env.REACT_APP_API_URL+"/user", {
-        headers: {
-          "auth-token": cookies.user,
-        },
-      })
-      .then((res) => {
-        console.log(res.data);
-        setData(res.data);
-        setUserinfo(res.data.user);
-        setWorkout(res.data.user.workout);
-        console.log(res.data.user.workout);
-        setError(null);
-      })
-      .catch((err) => {
-        console.log(err.message);
-        setError(err.message);
-        setData(null);
-        navigate("/login", { state: data });
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  }, []);
+    useEffect(() => {
+        // User data fetching here
+        axios
+            .get(process.env.REACT_APP_API_URL + "/user", {
+                headers: {
+                    "auth-token": cookies.user,
+                },
+            })
+            .then((res) => {
+                console.log(res.data);
+                setData(res.data);
+                setUserinfo(res.data.user);
+                setWorkout(res.data.user.workout);
+                console.log(res.data.user.workout);
+                setError(null);
+            })
+            .catch((err) => {
+                console.log(err.message);
+                setError(err.message);
+                setData(null);
+                navigate("/login", { state: data });
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    }, []);
 
-  const navigate = useNavigate();
-  const toCreateWorkout = () => {
-    navigate("/create-workouts", { state: data });
-  };
+    const navigate = useNavigate();
+    const toCreateWorkout = () => {
+        navigate("/create-workouts", { state: data });
+    };
 
-  function Logout() {
-    if (cookies.user) {
-      removeCookie("user");
+    function Logout() {
+        if (cookies.user) {
+            removeCookie("user");
+        }
+        navigate("/login");
     }
-    navigate("/login");
-  }
-  return (
-    <div>
-      {loading && (
-        <div className="loadingContainer">
-          <CircularProgress />
+    return (
+        <div>
+            {loading && (
+                <div className="loadingContainer">
+                    <CircularProgress />
+                </div>
+            )}
+            {error && (
+                <div>{`There is a problem fetching user data - ${error}`}</div>
+            )}
+            {data && (
+                <header className="header-main-page-container">
+                    <h1>{data.user.name}</h1>
+                    <Button
+                        variant="contained"
+                        className="btAddTraining gradientButton"
+                        onClick={() => {
+                            toCreateWorkout();
+                        }}
+                    >
+                        Create a Workout
+                    </Button>
+                    <Button
+                        variant="contained"
+                        className="btLogout"
+                        onClick={Logout}
+                    >
+                        <LockIcon className="icon-in-button" />
+                        Logout
+                    </Button>
+
+                    <div className="workoutsComponentsContainer">
+                        {workout &&
+                            workout.map((id: any) => (
+                                <Workout
+                                    workout_id={id}
+                                    key={id}
+                                    user={data.user}
+                                    notify={notify}
+                                    setNotify={setNotify}
+                                />
+                            ))}
+                    </div>
+                </header>
+            )}
+
+            <Notification notify={notify} setNotify={setNotify} />
         </div>
-      )}
-      {error && <div>{`There is a problem fetching user data - ${error}`}</div>}
-      {data && (
-        <header className="headerMain-pageContainer">
-          <h1>{data.user.name}</h1>
-          <Button
-            variant="contained"
-            className="btAddTraining gradientButton"
-            onClick={() => {
-              toCreateWorkout();
-            }}
-          >
-            Create a Workout
-          </Button>
-          <Button variant="contained" className="btLogout" onClick={Logout}>
-            Logout
-          </Button>
-
-          <div className="workoutsComponentsContainer">
-            {workout &&
-              workout.map((id: any) => (
-                <Workout
-                  workout_id={id}
-                  key={id}
-                  user={data.user}
-                  notify={notify}
-                  setNotify={setNotify}
-                />
-              ))}
-          </div>
-        </header>
-      )}
-
-      <Notification notify={notify} setNotify={setNotify} />
-    </div>
-  );
+    );
 }
 
 export default MainPage;
